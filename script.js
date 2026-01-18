6517a4986cdca44c01899c8fcf15e7c56bc08943
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-18 20:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const button = document.querySelector('input[type="button"]');
+
+button.onclick = function () {
+  const select = document.getElementById("colorSelect");
+  const selectedIndex = select.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+};
